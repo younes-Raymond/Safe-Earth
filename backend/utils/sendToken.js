47784c6b0658bfe225780e2a3cb1f5bd853b6d97
younes1337 <raymondyounes2@gmail.com>
2023-09-15 11:58:21 +0100

    the first commit change

--- backend/utils/sendToken.js
+++ backend/utils/sendToken.js
@@ -8,6 +8,7 @@ const sendToken = (user, statusCode, res) => {
         httpOnly: true
     }
 
+
     res.status(statusCode).cookie('token', token, options).json({
         success: true,
         user,
